4274e662db16cb7b2a79767ee135be08344b3566
Guy Marshall <guymarshall9@gmail.com>
2022-11-14 21:52:17 +0000

    Move script code to script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+function playA() {
+    var audio = document.getElementById("noteA");
+    audio.play();
+}
+function playB() {
+    var audio = document.getElementById("noteB");
+    audio.play();
+}
+function playC() {
+    var audio = document.getElementById("noteC");
+    audio.play();
+}
+function playD() {
+    var audio = document.getElementById("noteD");
+    audio.play();
+}
+function playE() {
+    var audio = document.getElementById("noteE");
+    audio.play();
+}
+function playF() {
+    var audio = document.getElementById("noteF");
+    audio.play();
+}
+function playG() {
+    var audio = document.getElementById("noteG");
+    audio.play();
+}
