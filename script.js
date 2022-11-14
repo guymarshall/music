1e8f4f7e90e2a449897b54e753058c8ee43611e9
Guy Marshall <guymarshall9@gmail.com>
2022-11-14 21:56:39 +0000

    Convert to arrow functions

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-document.getElementById("buttonA").addEventListener("click", function() {
-    document.getElementById("noteA").play();
-});
-document.getElementById("buttonB").addEventListener("click", function() {
-    document.getElementById("noteB").play();
-});
-document.getElementById("buttonC").addEventListener("click", function() {
-    document.getElementById("noteC").play();
-});
-document.getElementById("buttonD").addEventListener("click", function() {
-    document.getElementById("noteD").play();
-});
-document.getElementById("buttonE").addEventListener("click", function() {
-    document.getElementById("noteE").play();
-});
-document.getElementById("buttonF").addEventListener("click", function() {
-    document.getElementById("noteF").play();
-});
-document.getElementById("buttonG").addEventListener("click", function() {
-    document.getElementById("noteG").play();
-});
+document.getElementById("buttonA").addEventListener("click", () => document.getElementById("noteA").play());
+document.getElementById("buttonB").addEventListener("click", () => document.getElementById("noteB").play());
+document.getElementById("buttonC").addEventListener("click", () => document.getElementById("noteC").play());
+document.getElementById("buttonD").addEventListener("click", () => document.getElementById("noteD").play());
+document.getElementById("buttonE").addEventListener("click", () => document.getElementById("noteE").play());
+document.getElementById("buttonF").addEventListener("click", () => document.getElementById("noteF").play());
+document.getElementById("buttonG").addEventListener("click", () => document.getElementById("noteG").play());
